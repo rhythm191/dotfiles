f2a9240d853dd2246a5e21fa01d0940aa0cb326e
rhythm191 <maneater.rhythm@gmail.com>
2013-02-28 16:18:25 +0900

    setupのモジュールを追加

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc)
+DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf )
 
 for file in ${DOT_FILES[@]}
 do
