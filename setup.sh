6985ec63099cc56fd9a3eddb9b71aebd02814441
rhythm191 <maneater.rhythm@gmail.com>
2013-04-25 19:32:00 +0900

    gemrcを追加

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf )
+DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc)
 
 for file in ${DOT_FILES[@]}
 do
