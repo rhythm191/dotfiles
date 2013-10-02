38968dd3c65d7dbd9e52d355ba423d00aa9692bb
rhythm191 <maneater_rhythm@yahoo.co.jp>
2013-10-02 11:18:46 +0900

    vimの設定を追加(途中

---
+++ .vimrc
@@ -0,0 +1,16 @@
+" vim setting
+
+" no compatible mode
+set nocompatible
+
+" setting bundle
+source ~/dotfiles/.vimrc.bundle
+
+" setting basic
+source ~/dotfiles/.vimrc.basic
+
+" tab
+source ~/dotfiles/.vimrc.tab
+
+" status line
+source ~/dotfiles/.vimrc.status
--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc)
+DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc, .vimrc)
+
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim.git neobundle.vim
 
 for file in ${DOT_FILES[@]}
 do
