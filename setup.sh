cb55439c4e7ab55577c41036b05efacaf1c03529
rhythm191 <maneater_rhythm@yahoo.co.jp>
2013-10-02 14:08:36 +0900

    status画面がいい感じになった

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc, .vimrc)
 
 mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim.git neobundle.vim
+git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
 
 for file in ${DOT_FILES[@]}
 do
-ln -sf $HOME/dotfiles/$file $HOME
+  ln -sf $HOME/dotfiles/$file $HOME
 done
