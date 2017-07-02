2f1da719a4b8d69f7e2febf1c49413cb43e20c33
rhythm191 <sac2ndg+develop@gmail.com>
2017-07-02 15:59:28 +0900

    gitの補完機能を追加

--- setup.sh
+++ setup.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .bashrc .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc, .vimrc)
-
-mkdir -p ~/.vim/bundle
-git clone https://github.com/Shougo/neobundle.vim.git ~/.vim/bundle/neobundle.vim
+DOT_FILES=( .bashrc .zsh .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc, .vimrc)
 
 for file in ${DOT_FILES[@]}
 do
