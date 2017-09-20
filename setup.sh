feef143260f88b8c38c2e0e2ce49120f52bed7e8
kimpara <keigo.kimpara@gi-no.jp>
2017-09-20 13:34:34 +0900

    fix setup gemrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .bashrc .zsh .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc, .vimrc)
+DOT_FILES=( .bashrc .zsh .zshrc .zshenv .emacs.d .gitconfig .screenrc .tmux.conf .gemrc .vimrc)
 
 for file in ${DOT_FILES[@]}
 do
